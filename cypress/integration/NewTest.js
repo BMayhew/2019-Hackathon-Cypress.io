587d2bcc0d243d8f0ca034f5df2378f3c97261ed
Butch Mayhew <bmayhew@daxko.com>
2019-11-15 00:39:33 -0600

    Finished cypress section 5

--- cypress/integration/NewTest.js
+++ cypress/integration/NewTest.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 
 beforeEach(function () {
-    //cy.visit('https://demo.applitools.com/hackathonV2.html')
     cy.visit('https://demo.applitools.com/hackathon.html')
+    // cy.visit('https://demo.applitools.com/hackathonV2.html')
 })
 
 //Custom Login Helper For Section 3
@@ -12,23 +12,41 @@ Cypress.Commands.add('login', () => {
     cy.get('#log-in').click()
 })
 
-
-it('should show proper advertisments', () => {
+Cypress.Commands.add('loginwadds', () => {
     cy.visit('https://demo.applitools.com/hackathon.html?showAd=true')
-    cy.login()
+    // cy.visit('https://demo.applitools.com/hackathonV2.html?showAd=true')
+    cy.get('form #username').type('Testy McTester')
+    cy.get('form #password').type('Passymcpassword!1')
+    cy.get('#log-in').click()
+})
+
+
+
+it('should show proper advertisments hard coded', () => {
+    cy.loginwadds()
     cy.get('.element-balances').within(() => {
         cy.get('#flashSale').should('include.html', 'src="img/flashSale.gif"')
         cy.get('#flashSale2').should('include.html', 'src="img/flashSale2.gif"')
     })
 })
 
-it('should show proper advertisments', () => {
-    cy.visit('https://demo.applitools.com/hackathonV2.html?showAd=true')
-    cy.login()
-    cy.get('.element-balances').within(() => {
-        cy.get('#flashSale2').should('include.html', 'src="img/flaseSale3.gif"')
+it('should show atleast 2 advertisements ', () => {
+    cy.loginwadds()
+    cy.get('.element-balances').find('img')
+    .should(($img) => {
+      expect($img).to.have.length(2)
     })
 })
 
-//TODO, make Section 5 Tests smarter
-//TODO, Visualization Tests
+it('should support dynamic proper advertisments', () => {
+    cy.loginwadds()
+    cy.get('.element-balances').find('img')
+    .should(($img) => {
+  
+      const imageSrc0 = $img[0].src
+  
+      expect(imageSrc0).to.match(/\/img\/flashSale*..gif/)
+    })
+})
+
+
--- cypress/integration/TraditionalTests.js
+++ cypress/integration/TraditionalTests.js
@@ -1,12 +1,21 @@
 /// <reference types="cypress" />
 
 beforeEach(function () {
-    cy.visit('https://demo.applitools.com/hackathonV2.html')
-    //cy.visit('https://demo.applitools.com/hackathon.html')
+    //cy.visit('https://demo.applitools.com/hackathonV2.html')
+    cy.visit('https://demo.applitools.com/hackathon.html')
 })
 
 //Custom Login Helper For Section 3
 Cypress.Commands.add('login', () => {
+    cy.get('form #username').type('Testy McTester')
+    cy.get('form #password').type('Passymcpassword!1')
+    cy.get('#log-in').click()
+})
+
+//Custom Login Helper for Adds
+Cypress.Commands.add('loginwadds', () => {
+    cy.visit('https://demo.applitools.com/hackathon.html?showAd=true')
+    // cy.visit('https://demo.applitools.com/hackathonV2.html?showAd=true')
     cy.get('form #username').type('Testy McTester')
     cy.get('form #password').type('Passymcpassword!1')
     cy.get('#log-in').click()
@@ -210,7 +219,29 @@ it('should show next years data in expenses chart', () => {
 })
 
 //Section 5
-it('should show proper advertisments', () => {
-    cy.visit('https://demo.applitools.com/hackathon.html?showAd=true')
-    cy.login()
+it('should show proper advertisments hard coded', () => {
+    cy.loginwadds()
+    cy.get('.element-balances').within(() => {
+        cy.get('#flashSale').should('include.html', 'src="img/flashSale.gif"')
+        cy.get('#flashSale2').should('include.html', 'src="img/flashSale2.gif"')
+    })
+})
+
+it('should show atleast 2 advertisements ', () => {
+    cy.loginwadds()
+    cy.get('.element-balances').find('img')
+    .should(($img) => {
+      expect($img).to.have.length(2)
+    })
+})
+
+it('should support dynamic proper advertisments', () => {
+    cy.loginwadds()
+    cy.get('.element-balances').find('img')
+    .should(($img) => {
+  
+      const imageSrc0 = $img[0].src
+  
+      expect(imageSrc0).to.match(/\/img\/flashSale*..gif/)
+    })
 })
